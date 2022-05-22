9eb42b2bfbf309e0108bc757f88b05806788589f
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2022-05-23 00:59:17 +0300

    Final push of the project, sorry for the late push, it took me some time to write the documentation and wrap things up

--- class_file.py
+++ class_file.py
@@ -1,11 +1,20 @@
 import pygame
-import sys
 
 
+# Create Enemy class
 class Enemy:
     def __init__(self):
+        """
+        Holds value of price for killing an enemy,
+        wave level
+        enemy unit health per wave
+        counter for change_coord method calls
+        enemy image
+        list of enemy coordinates
+        """
+        self.money = 15
         self.wave = 1
-        self.health = [self.wave for i in range(30)]
+        self.health = [self.wave*2 for i in range(30)]
         self.loop_counter = 0
         self.enemy_img = pygame.image.load("game_enemy_img.png").convert_alpha()
         self.enemies_coord = []
@@ -14,6 +23,7 @@ class Enemy:
             self.enemies_coord.append([-100 - idx, 150])
             idx += 100
 
+    # Changes enemy coordinates
     def change_coord(self):
         if self.loop_counter < 20:
             self.loop_counter += 1
@@ -21,15 +31,16 @@ class Enemy:
         for idx in range(30):
             if self.enemies_coord[idx][1] >= 730:
                 self.loop_counter = 0
-                self.enemies_coord[idx][0] -= 10
+                self.enemies_coord[idx][0] -= (10*self.wave)
             elif self.enemies_coord[idx][0] >= 1200:
                 self.loop_counter = 0
-                self.enemies_coord[idx][1] += 10
+                self.enemies_coord[idx][1] += (10*self.wave)
             else:
                 self.loop_counter = 0
-                self.enemies_coord[idx][0] += 10
+                self.enemies_coord[idx][0] += (10*self.wave)
         self.loop_counter += 1
 
+    # Enemies take damage when method called
     def loose_health(self, enemy_coord, damage_taken):
         for idx in range(len(self.enemies_coord)):
             if self.enemies_coord[idx] == enemy_coord:
@@ -37,11 +48,7 @@ class Enemy:
                     self.enemies_coord[idx] = [-10000, -10000]
                 self.health[idx] -= damage_taken
 
-    def deal_damage(self, take_damage):
-        for idx in range(len(self.enemies_coord)):
-            if self.enemies_coord[idx] == [-10, 730]:
-                take_damage(self.health[idx])
-
+    # Spawns a new UPGRADED wave
     def new_wave(self):
         if self.enemies_coord[29] == [-10, 730] or self.enemies_coord[29] == [-10000, -10000]:
             self.enemies_coord.clear()
@@ -51,17 +58,28 @@ class Enemy:
                 idx += 100
             self.wave += 1
             self.health.clear()
-            self.health = [self.wave for i in range(30)]
+            self.health = [self.wave*2 for i in range(30)]
 
 
+# Create Tower class
 class Tower:
     def __init__(self):
+        """
+        Sets Tower price
+        Sets upgrade price
+        Sets loop counter for attack_enemy method
+        Sets tower projectile image
+        Sets a dict of all values held from a tower
+        Makes a list for the dictionaries of the towers
+        """
         self.price = 200
         self.upgrade_price = 250
+        self.loop_counter = 0
         self.projectile = pygame.image.load("game_projectile_img.png").convert_alpha()
         self.tower_dict = {"height": 200, "width": 180, "dmg": 5, "range_x": 450, "range_y": 450}
         self.tower_list = []
 
+    # Upgrade tower stats
     def upgrade_tower(self, position):
         for idx in range(len(self.tower_list)):
             if self.tower_list[idx][1] == position:
@@ -76,6 +94,7 @@ class Tower:
                     self.tower_list[idx][0]["range_y"] += 50
                     return
 
+    # Check if tower x, y overlaps with the x, y of a given target ( mouse position or other tower x, y coordinates )
     def check_for_input(self, position):
         for idx in range(len(self.tower_list)):
             if self.tower_list[idx][1] == position:
@@ -87,12 +106,17 @@ class Tower:
                     return True
             return False
 
+    # Checks if enemy is in tower range
     def attack_enemy(self, position):
+        if self.loop_counter < 15:
+            self.loop_counter += 1
+            return
         for pos in self.tower_list:
             if position[0] in range((pos[1][0] - pos[0]["range_x"]), (pos[1][0] + pos[0]["range_x"])) and \
                     position[1] in range((pos[1][1] - pos[0]["range_y"]), (pos[1][1] + pos[0]["range_y"])):
                 return True
 
+    # Shoots a projectile
     def shoot_projectile(self, position, screen):
         for pos in self.tower_list:
             if position[0] in range((pos[1][0] - pos[0]["range_x"]), (pos[1][0] + pos[0]["range_x"])) and \
@@ -102,6 +126,7 @@ class Tower:
                 screen.blit(self.projectile, (projectile_position_x, projectile_position_y))
 
 
+# Create varius classes from base Tower class with little changes to base parameters
 class YellowTower(Tower):
     def __init__(self):
         Tower.__init__(self)
@@ -109,15 +134,17 @@ class YellowTower(Tower):
         self.yellow_img = pygame.image.load("game_tower_one_img.png").convert_alpha()
 
 
+# Create varius classes from base Tower class with little changes to base parameters
 class BlueTower(Tower):
     def __init__(self):
         Tower.__init__(self)
-        self.price = 250
+        self.price = 50
         self.range_x = 600
         self.range_y = 600
         self.blue_img = pygame.image.load("game_tower_two_img.png").convert_alpha()
 
 
+# Create varius classes from base Tower class with little changes to base parameters
 class GreenTower(Tower):
     def __init__(self):
         Tower.__init__(self)
@@ -127,16 +154,28 @@ class GreenTower(Tower):
         self.green_img = pygame.image.load("game_tower_three_img.png").convert_alpha()
 
 
+# Create a button class for all buttons in game
 class Button:
     def __init__(self, x, y, image, width, height):
+        """
+        __init__ constructor method
+        :param x: x coord
+        :param y: y coord
+        :param image: image for button
+        :param width: width of button
+        :param height: height of button
+        """
         self.image = image
+        # Creates a rectangle box object for the image and button
         self.rect = self.image.get_rect(topleft=(x, y))
         self.width = width
         self.height = height
 
+    # Draws the button
     def draw(self, screen):
         screen.blit(pygame.transform.scale(self.image, (self.width, self.height)), (self.rect.x, self.rect.y))
 
+    # Checks if button is pressed by given position and button x, y and width, and height coordinates
     def check_for_input(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top,
                                                                                           self.rect.bottom):
@@ -144,18 +183,42 @@ class Button:
         return False
 
 
+# Create a class for the main features of the player
 class GameMenu:
     def __init__(self):
+        """
+        Sets base health
+        Sets Base money
+        Makes a constant for the RGB value of the colour white
+        Sets a font for text printing
+        """
         self.player_health = 100
         self.player_money = 300
+        self.WHITE = (255, 255, 255)
+        self.font = pygame.font.Font(None, 50)
 
+    # Player takes damage if enemy crosses finish line
     def take_damage(self, damage):
-        if self.player_health <= damage:
-            pygame.quit()
-            sys.exit()
-        self.player_health -= damage
+        self.player_health = self.player_health - damage
 
+    # Prints player health
+    def get_health(self, screen):
+        text_health = self.font.render("Health: " + str(self.player_health), True, self.WHITE)
+        text_health_rect = text_health.get_rect(center=(1375, 90))
+        screen.blit(text_health, text_health_rect)
+
+    # Prints player money
+    def get_money(self, screen):
+        text_money = self.font.render("Money: " + str(self.player_money), True, self.WHITE)
+        text_money_rect = text_money.get_rect(center=(1375, 140))
+        screen.blit(text_money, text_money_rect)
+
+    # Prints wave level
+    def get_wave(self, screen, wave):
+        text_wave = self.font.render("Wave: " + str(wave), True, self.WHITE)
+        text_wave_rect = text_wave.get_rect(center=(1375, 190))
+        screen.blit(text_wave, text_wave_rect)
+
+    # Player money drops after a purchase
     def loose_money(self, cost):
-        if self.player_money < cost:
-            return True
         self.player_money -= cost
--- game.py
+++ game.py
@@ -3,18 +3,15 @@ import pygame
 import sys
 from class_file import Enemy, GameMenu, Button, YellowTower, BlueTower, GreenTower
 
+# Create constant values for screen width and height
 SCREEN_WIDTH = 1500
 SCREEN_HEIGHT = 950
 
+# Start a pygame window and sett its size
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-def game_start():
-    pygame.init()
-    surface_init = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    return surface_init
-
-
-screen = game_start()
-
+# Load needed images
 background_img = pygame.image.load("game_background_img.png").convert_alpha()
 menu_background_img = pygame.image.load("menu_screen.png").convert_alpha()
 load_img = pygame.image.load("load.png").convert_alpha()
@@ -22,23 +19,29 @@ save_img = pygame.image.load("save-button-icon.png").convert_alpha()
 buy_yellow_img = pygame.image.load("buy_yellow.png").convert_alpha()
 buy_blue_img = pygame.image.load("buy_blue.png").convert_alpha()
 
+# Set a clock to manipulate game run speed
 clock = pygame.time.Clock()
 
+# Create objects of the Button class with the loaded images
 buy_yellow_button = Button(1350, 690, buy_yellow_img, 150, 80)
 buy_blue_button = Button(1350, 790, buy_blue_img, 150, 80)
 save_button = Button(20, 25, save_img, 100, 60)
 load_button = Button(550, 480, load_img, 300, 250)
 
+# Create objects of the classes needed ti run a game
 enemy = Enemy()
 yellow_tower = YellowTower()
 blue_tower = BlueTower()
+green_tower = GreenTower()
 game_menu = GameMenu()
 
 
+# Create a main menu loop where you can load a new or pre-existing game
 def menu_main():
     # Sets pygame window caption to "Main menu"
     pygame.display.set_caption("Main menu")
 
+    # Main menu loop
     while True:
         # Gets mouse position
         mouse_pos = pygame.mouse.get_pos()
@@ -48,22 +51,28 @@ def menu_main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            # If load button is pressed, the game is loaded from information from file
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if load_button.check_for_input(mouse_pos):
-                    file_one = open("/game_data_saves.txt", "r")
+                    # Open and read and load info from file
+                    file_one = open("/game_data_saves.txt", "r+")
                     line = file_one.readline()
-                    file_one.close()
                     data = line.split(";")
                     enemy.wave = int(data[0])
                     game_menu.player_money = int(data[1])
                     game_menu.player_health = int(data[2])
+                    # Close file
+                    file_one.close()
+                    # Enter game
                     game_main()
 
+        # Print and draw Main menu graphics on screen
         screen.blit(pygame.transform.scale(menu_background_img, (1500, 950)), (0, 0))
         load_button.draw(screen)
         pygame.display.update()
 
 
+# Create a game loop and Game menu to play, when save button pressed, save info in file and go to Main menu
 def game_main():
     # Sets pygame window caption to "Game menu"
     pygame.display.set_caption("Game menu")
@@ -72,13 +81,19 @@ def game_main():
     click_count_yellow = 0
     # Variable checks how many mouse clicks there were made for blue towers
     click_count_blue = 0
+    # Variable that signals that there was a yellow tower purchase done
+    can_buy_yellow = 0
+    # Variable that signals that there was a blue tower purchase done
+    can_buy_blue = 0
+
+    # Game menu loop
     while True:
         # Gets mouse position
         mouse_position = pygame.mouse.get_pos()
         # Checks for any events that occur during runtime, like mouse clicks
         for event in pygame.event.get():
-            # Checks if pygame window is quit, if it is then the game and program stop running
-            if event.type == pygame.QUIT:
+            # Checks if pygame window is quit or player is dead, if it is then the game and program stop running
+            if event.type == pygame.QUIT or game_menu.player_health <= 0:
                 pygame.quit()
                 sys.exit()
             # Checks if there is a mouse click
@@ -91,21 +106,45 @@ def game_main():
                     file_two.close()
                     menu_main()
 
-                # Checks for input on yellow tower buy button and gives bought tower coordinates of second mouse press
-                if buy_yellow_button.check_for_input(mouse_position):
-                    click_count_yellow += 1
-                    game_menu.loose_money(yellow_tower.price)
-                if event.button == 3 and click_count_yellow >= 1:
-                    yellow_tower.tower_list.append([yellow_tower.tower_dict, mouse_position])
-                    click_count_yellow = 0
+                # Checks if shop can be accessed based on money and purchase history
+                if game_menu.player_money > 250 or can_buy_yellow >= 1:
 
-                # Checks for input on blue tower buy button and gives bought tower coordinates of second mouse press
-                if buy_blue_button.check_for_input(mouse_position):
-                    click_count_blue += 1
-                    game_menu.loose_money(blue_tower.price)
-                if event.button == 3 and click_count_blue >= 1:
-                    blue_tower.tower_list.append([yellow_tower.tower_dict, mouse_position])
-                    click_count_blue = 0
+                    # Checks for input on yellow tower buy button and gives coordinates of second mouse press to tower
+                    if buy_yellow_button.check_for_input(mouse_position) and can_buy_yellow < 1:
+                        click_count_yellow += 1
+                        game_menu.loose_money(yellow_tower.price)
+                        can_buy_yellow += 1
+                    # If right click pressed, mouse coordinates are given
+                    if event.button == 3 and click_count_yellow >= 1:
+                        # If towers don't overlap they get placed on already given mouse coordinates
+                        if not yellow_tower.check_for_input(mouse_position) or \
+                                buy_blue_button.check_for_input(mouse_position):
+                            yellow_tower.tower_list.append([yellow_tower.tower_dict, mouse_position])
+                            click_count_yellow = 0
+                            can_buy_yellow = 0
+                        # If towers overlap they spawn a green buffed tower on given mouse coordinates
+                        else:
+                            green_tower.tower_list.append([yellow_tower.tower_dict, mouse_position])
+
+                # Checks if shop can be accessed based on money and purchase history
+                if game_menu.player_money > 250 or can_buy_blue >= 1:
+
+                    # Checks for input on blue tower buy button and gives coordinates of second mouse press to tower
+                    if buy_blue_button.check_for_input(mouse_position) and can_buy_blue < 1:
+                        click_count_blue += 1
+                        game_menu.loose_money(blue_tower.price)
+                        can_buy_blue += 1
+                    # If right click pressed, mouse coordinates are given
+                    if event.button == 3 and click_count_blue >= 1:
+                        # If towers don't overlap they get placed on already given mouse coordinates
+                        if not yellow_tower.check_for_input(mouse_position) or \
+                                buy_blue_button.check_for_input(mouse_position):
+                            blue_tower.tower_list.append([yellow_tower.tower_dict, mouse_position])
+                            click_count_blue = 0
+                            can_buy_blue = 0
+                        # If towers overlap they spawn a green buffed tower on given mouse coordinates
+                        else:
+                            green_tower.tower_list.append([yellow_tower.tower_dict, mouse_position])
 
                 # Checks if yellow tower is pressed and if it is, it gets upgraded
                 if yellow_tower.check_for_input(mouse_position) and event.button == 1:
@@ -115,27 +154,68 @@ def game_main():
                 if blue_tower.check_for_input(mouse_position) and event.button == 1:
                     blue_tower.upgrade_tower(mouse_position)
 
+        # All images are printed on screen and all functions from classes are run and all checks for firing projectiles
+        # and for current money, overlapping enemy and tower coordinates and wave number are made
+
+        # Prints background
         screen.blit(background_img, (0, 0))
+        # Prints tower buy buttons
         buy_yellow_button.draw(screen)
         buy_blue_button.draw(screen)
+        # Changes enemy coordinates and moves them
         enemy.change_coord()
+        # Prints enemies on screen
         for coordinates in enemy.enemies_coord:
             screen.blit(pygame.transform.scale(enemy.enemy_img, (100, 90)), coordinates)
+        # Prints yellow towers on screen
         for yellow in yellow_tower.tower_list:
             screen.blit(pygame.transform.scale(yellow_tower.yellow_img, (yellow[0]["width"], yellow[0]["height"])), yellow[1])
+        # Prints blue towers on screen
         for blue in blue_tower.tower_list:
             screen.blit(pygame.transform.scale(blue_tower.blue_img, (blue[0]["width"], blue[0]["height"])), blue[1])
+        # Prints green towers on screen
+        for green in green_tower.tower_list:
+            screen.blit(pygame.transform.scale(green_tower.green_img, (green[0]["width"], green[0]["height"])), green[1])
+        # Checks if enemy is in yellow tower range and if it is, tower fires projectile and player gains money
         for position in enemy.enemies_coord:
             if yellow_tower.attack_enemy(position):
                 yellow_tower.shoot_projectile(position, screen)
                 enemy.loose_health(position, yellow_tower.tower_dict["dmg"])
+                yellow_tower.loop_counter += 1
+                game_menu.player_money += enemy.money
+        # Checks if enemy is in blue tower range and if it is, tower fires projectile and player gains money
         for position in enemy.enemies_coord:
             if blue_tower.attack_enemy(position):
                 blue_tower.shoot_projectile(position, screen)
                 enemy.loose_health(position, blue_tower.tower_dict["dmg"])
+                blue_tower.loop_counter += 1
+                game_menu.player_money += enemy.money
+        # Checks if enemy is in green tower range and if it is, tower fires projectile and player gains money
+        for position in enemy.enemies_coord:
+            if green_tower.attack_enemy(position):
+                green_tower.shoot_projectile(position, screen)
+                enemy.loose_health(position, green_tower.tower_dict["dmg"])
+                green_tower.loop_counter += 1
+                game_menu.player_money += enemy.money
+        # Checks if enemies crossed the finish line
+        for idx in range(len(enemy.enemies_coord)):
+            if enemy.enemies_coord[idx] == [-10, 730]:
+                game_menu.take_damage(enemy.health[idx])
+        # Raises enemy wave level if needed
         enemy.new_wave()
+        # Prints player health
+        game_menu.get_health(screen)
+        # Prints player money
+        game_menu.get_money(screen)
+        # Prints wave level
+        game_menu.get_wave(screen, enemy.wave)
+        # Prints Save button
         save_button.draw(screen)
+        # Updates Screen
         pygame.display.update()
+        # Sets pygame loop speed
         clock.tick(120)
 
-game_main()
+
+# Calls Main menu when program starts running
+menu_main()
